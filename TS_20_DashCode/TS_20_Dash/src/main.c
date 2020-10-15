64c1eaed102c5427a13aa30924b763023f21fbd6
Christian <102090647@student.swin.edu.au>
2020-10-15 16:28:10 +1100

    UPDATE: added splash screen code looks dodgy but works so please dont break, also can destroy the splash screen after loop in main

--- TS_20_DashCode/TS_20_Dash/src/main.c
+++ TS_20_DashCode/TS_20_Dash/src/main.c
@@ -12,28 +12,41 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "lvgl.h"
 #include "driver.h"
 
 #include "menu.h"
+#include "splash.h"
 
 #include "screen1.h"
 #include "screen2.h"
 #include "screen3.h"
+
 
 int main(void)
 {
 	lv_init();
 	hw_init();
   
-
+  load_splash();
   //I wanna make a splash screen, that function will go here.
   //splashScreen(blah blah);
-
+  int i = 0;
+  while(i < 500)
+  {
+      // delay of one second 
+      lv_task_handler();
+      usleep(10000);
+      i++;
+        //printf("%d seconds have passed\n", i + 1); 
+  } 
   //runs the main menu, the hub of our dashboard.
   menuInit(lv_theme_night_init(63488, NULL));
   //screen2Init(lv_theme_night_init(63488, NULL));
  
 	hw_loop();
+
+  return 0;
 }
