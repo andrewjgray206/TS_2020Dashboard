52a7328447c5c4a08210b78743f30ac0f0f2f033
TanselKahramanSwinburne <100576844@student.swin.edu.au>
2020-10-16 12:29:15 +1100

    Delay changes.

--- TS_20_DashCode/TS_20_Dash/src/main.c
+++ TS_20_DashCode/TS_20_Dash/src/main.c
@@ -24,6 +24,8 @@
 #include "screen2.h"
 #include "screen3.h"
 
+#include <unistd.h>
+
 
 int main(void)
 {
@@ -31,17 +33,16 @@ int main(void)
 	hw_init();
   
   load_splash();
-  //I wanna make a splash screen, that function will go here.
-  //splashScreen(blah blah);
-  int i = 0;
-  while(i < 500)
-  {
-      // delay of one second 
-      lv_task_handler();
-      usleep(10000);
-      i++;
-        //printf("%d seconds have passed\n", i + 1); 
-  } 
+
+  for (int i = 0; i < 2000; i++) {
+    lv_task_handler();
+    HAL_Delay(1);
+  }
+   
+
+  
+  
+
   //runs the main menu, the hub of our dashboard.
   menuInit(lv_theme_night_init(63488, NULL));
   //screen2Init(lv_theme_night_init(63488, NULL));
